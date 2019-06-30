7c979d0c45df44158d75a5b398687216bcd5c3bc
Simone Robutti <simone.robutti@protonmail.com>
2019-06-30 21:10:26 +0200

    end of beta (#65)
    * Move "filters" link from menu to user settings
    
    * added blank to hat links
    
    * added presenta tag
    
    * fixed css and descriptions
    
    * Fix some template bugs (#59)
    
    * Convert database to utf8mb4 (#61)
    
    * Add migration to convert all db to utf8mb4
    
    * Improve migration and commit schema
    
    * Template fixes 7 (#62)
    
    * fixed error strings
    
    * changed ordering of received and sent messages
    
    * fixed more error messages
    
    * now distinguishing between pubblicato da and scritto da
    
    * Enable HSTS (#64)
    
    * Exit beta test (#56)
    
    * Remove references to beta test
    
    * Exit closed testing

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ class << Rails.application
   # when true limits the number of invitation sendings to max_invitations_count
   # when false there is no limit on the number of invitation sendings
   def closed_testing?
-    true
+    false
   end
 
   # the maximum number of invitation sendings when closed_testing? is true
