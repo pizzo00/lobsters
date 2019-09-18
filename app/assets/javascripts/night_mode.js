556012c97fe9d82f2b7b1da5022287d17a021670
Simone Robutti <simone.robutti@protonmail.com>
2019-09-18 02:35:15 +0200

    added day/night switch

--- app/assets/javascripts/night_mode.js
+++ app/assets/javascripts/night_mode.js
@@ -1,19 +1,37 @@
 function set_night_mode(){
-	Cookies.set("night_mode","true")
+	Cookies.set("night_mode",true)
 	document.documentElement.setAttribute('data-theme', 'dark')
+	$("#input-nightswitch").prop("checked",true)
+
+
 }
 
 function set_day_mode(){
-	Cookies.set("night_mode","false")
+	Cookies.set("night_mode",false)
 	document.documentElement.setAttribute('data-theme', 'light')
+	$("#input-nightswitch").prop("checked",false)
+}
+
+function set_mode(night_mode){
+	if(night_mode){
+		console.log("goodnight")
+		set_night_mode();
+				}
+	else{
+		console.log("good morning")
+		set_day_mode();
+		}
+
+
 }
 
 $(document).ready(function() {
 	var night_mode = Cookies.get("night_mode")
-	if(night_mode == "true"){
-		set_night_mode();
-		}
-	else{
-		set_day_mode();
-		}
+
+	set_mode(night_mode=="true")
+	
+	$("#input-nightswitch").on("change",function() {
+   		set_mode(this.checked)
+	});
+
 });
