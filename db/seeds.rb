d08ccf649d640953f9b9fc822256b70f888c14bc
Simone Robutti <simone.robutti@gmail.com>
2019-11-12 22:44:35 +0100

    fixed tags color and expanded seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 User.create(:username => "test", :email => "test@example.com", :password => "test", :password_confirmation => "test", :is_admin => true, :is_moderator => true)
 puts "created user: test, password: test"
 Tag.create(:tag => "test")
+Tag.create(:tag => "test2", :is_media=>true)
+Tag.create(:tag => "domanda")
+Tag.create(:tag => "meta")
 puts "created tag: test"
 User.create(:username => "test2", :email => "test@example.co2m", :password => "test", :password_confirmation => "test", :is_admin => true, :is_moderator => true)
 puts "created user: test2, password: test2"
