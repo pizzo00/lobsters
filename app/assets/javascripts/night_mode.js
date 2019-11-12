85ba2d5fcd7433818b9fc348e603ae7d8e824588
riggraz <riccardo.graziosi97@gmail.com>
2019-11-12 14:32:54 +0100

    Use localStorage instead of jscookie lib

--- app/assets/javascripts/night_mode.js
+++ app/assets/javascripts/night_mode.js
@@ -1,13 +1,13 @@
 function set_mode(night_mode)
 {
     mode = night_mode ? 'dark' :'light'
-    Cookies.set("night_mode", night_mode, { expires: 365 })
+    window.localStorage.setItem("night_mode", night_mode)
     document.documentElement.setAttribute('data-theme', mode)
     $("#fs").prop("checked",night_mode)
 }
 
 $(document).ready(function() {
-	var night_mode = Cookies.get("night_mode")
+	var night_mode = window.localStorage.getItem("night_mode")
 
 	set_mode(night_mode=="true")
 	
